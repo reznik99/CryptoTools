f6d7abe59eda9e605b5516a0f80d26f75f097de0
Francesco <goras.francesco@gmail.com>
2022-07-29 01:21:24 +1200

    Working AES, RSA and ECDSA Key generation

--- src/App.js
+++ src/App.js
@@ -1,48 +1,217 @@
 
+import React from 'react'
+
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import Spinner from 'react-bootstrap/Spinner';
 
 import './App.css';
 
-function App() {
-    return (
-        <div className="App" lg={6}>
-            <Container fluid>
-                <Row>
-                    <Col lg={3} style={{ padding: 0 }}>
-                        <Nav defaultActiveKey="AES" className="flex-column" style={{ backgroundColor: 'white', minHeight: '100vh' }} variant="pills">
-                            <h3 className="title">Crypto Tools</h3>
-                            <Nav.Item>Keys</Nav.Item>
-                            <Nav.Link eventKey="AES">AES</Nav.Link>
-                            <Nav.Link eventKey="RSA">RSA</Nav.Link>
-                            <Nav.Link eventKey="ECDSA">ECDSA</Nav.Link>
-                            <Nav.Item>Hashing</Nav.Item>
-                            <Nav.Link eventKey="SHA-1">SHA-1</Nav.Link>
-                            <Nav.Link eventKey="SHA-256">SHA-256</Nav.Link>
-                            <Nav.Link eventKey="SHA-384">SHA-384</Nav.Link>
-                            <Nav.Link eventKey="SHA-512">SHA-512</Nav.Link>
-                            <Nav.Item>Encoding</Nav.Item>
-                            <Nav.Link eventKey="Base64">Base64</Nav.Link>
-                            <Nav.Link eventKey="Hex">Hex</Nav.Link>
-                            <Nav.Link eventKey="Binary">Binary</Nav.Link>
-                        </Nav>
-                    </Col>
-                    <Col lg={9}>
-                        <h4> Generate Key </h4>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Keylength</Form.Label>
-                            <Form.Control type="numeric" placeholder="256" />
-                        </Form.Group>
-                        <Button>Generate</Button>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: false,
+            action: 'AES-Gen',
+            keyLength: 256,
+            curve: 'P-256',
+            input: '',
+        }
+    }
+
+    _arrayBufferToBase64 = (buffer) => {
+        var binary = '';
+        var bytes = new Uint8Array(buffer);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+    }
+
+    generateAES = async () => {
+        try {
+            this.setState({ loading: true })
+            const key = await window.crypto.subtle.generateKey(
+                {
+                    name: "AES-GCM",
+                    length: this.state.keyLength
+                },
+                true,
+                ["encrypt", "decrypt"]
+            );
+
+            const exported = await window.crypto.subtle.exportKey("raw", key)
+            const exportedKeyBuffer = new Uint8Array(exported);
+
+            this.setState({ generatedKey: this._arrayBufferToBase64(exportedKeyBuffer) })
+        } catch (err) {
+            console.error(err)
+        } finally {
+            this.setState({ loading: false })
+        }
+    }
+
+    generateRSA = async () => {
+        try {
+            this.setState({ loading: true })
+            const key = await window.crypto.subtle.generateKey(
+                {
+                    name: "RSA-OAEP",
+                    modulusLength: this.state.keyLength,
+                    publicExponent: new Uint8Array([1, 0, 1]),
+                    hash: "SHA-256"
+                },
+                true,
+                ["encrypt", "decrypt"]
+            );
+
+            const exported = await window.crypto.subtle.exportKey("pkcs8", key.privateKey)
+            const exportedAsBase64 = this._arrayBufferToBase64(new Uint8Array(exported))
+            const pemExported = `-----BEGIN PRIVATE KEY-----\n${exportedAsBase64}\n-----END PRIVATE KEY-----`;
+
+            this.setState({ generatedKey: pemExported })
+        } catch (err) {
+            console.error(err)
+        } finally {
+            this.setState({ loading: false })
+        }
+    }
+
+    generateECDSA = async () => {
+        try {
+            this.setState({ loading: true })
+            const key = await window.crypto.subtle.generateKey(
+                {
+                    name: "ECDSA",
+                    namedCurve: this.state.curve
+                },
+                true,
+                ["sign", "verify"]
+            );
+
+            const exported = await window.crypto.subtle.exportKey("pkcs8", key.privateKey)
+            const exportedAsBase64 = this._arrayBufferToBase64(new Uint8Array(exported))
+            const pemExported = `-----BEGIN PRIVATE KEY-----\n${exportedAsBase64}\n-----END PRIVATE KEY-----`;
+
+            this.setState({ generatedKey: pemExported })
+        } catch (err) {
+            console.error(err)
+        } finally {
+            this.setState({ loading: false })
+        }
+    }
+
+
+
+    render = () => {
+        return (
+            <div className="App" lg={6} >
+                <Container fluid>
+                    <Row>
+                        <Col lg={2} style={{ padding: 0 }}>
+                            <Nav activeKey={this.state.action} onSelect={eventKey => this.setState({ action: eventKey })} className="flex-column"
+                                style={{ backgroundColor: '#fff', height: '100vh', color: 'black', overflow: 'scroll' }} variant="pills">
+                                <h3 className="title">Crypto Tools</h3>
+                                <Nav.Item>Generation</Nav.Item>
+                                <Nav.Link eventKey="AES-Gen">AES Key</Nav.Link>
+                                <Nav.Link eventKey="RSA-Gen">RSA Keys</Nav.Link>
+                                <Nav.Link eventKey="ECDSA-Gen">ECDSA Keys</Nav.Link>
+                                <Nav.Link eventKey="CSR-Gen">CSR</Nav.Link>
+                                <Nav.Link eventKey="Certificate-Gen">Certificate</Nav.Link>
+                                <Nav.Item>Encryption/Decryption</Nav.Item>
+                                <Nav.Link eventKey="AES-Enc">AES</Nav.Link>
+                                <Nav.Link eventKey="RSA-Enc">RSA</Nav.Link>
+                                <Nav.Item>Signing/Validation</Nav.Item>
+                                <Nav.Link eventKey="RSA-Sig">RSA</Nav.Link>
+                                <Nav.Link eventKey="ECDSA-Sig">ECDSA</Nav.Link>
+                                <Nav.Item>Hashing</Nav.Item>
+                                <Nav.Link eventKey="SHA-1">SHA-1</Nav.Link>
+                                <Nav.Link eventKey="SHA-256">SHA-256</Nav.Link>
+                                <Nav.Link eventKey="SHA-384">SHA-384</Nav.Link>
+                                <Nav.Link eventKey="SHA-512">SHA-512</Nav.Link>
+                                <Nav.Item>Encoding</Nav.Item>
+                                <Nav.Link eventKey="Base64">Base64</Nav.Link>
+                                <Nav.Link eventKey="Hex">Hex</Nav.Link>
+                                <Nav.Link eventKey="Binary">Binary</Nav.Link>
+                            </Nav>
+                        </Col>
+                        <Col lg={5}>
+                            <Row style={{ height: '100vh' }} className="justify-content-md-center align-items-md-center">
+                                <Col lg={8}>
+                                    <h4> Generate Key </h4>
+
+                                    {this.state.action === 'AES-Gen' &&
+                                        <>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Keylength</Form.Label>
+                                                <Form.Control type="numeric" placeholder="256" value={this.state.keyLength} onChange={(e) => this.setState({ keyLength: Number(e.target.value) })} />
+                                            </Form.Group>
+                                            <Button onClick={this.generateAES}>Generate AES Key</Button>
+                                        </>
+                                    }
+                                    {this.state.action === 'RSA-Gen' &&
+                                        <>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Keylength</Form.Label>
+                                                <Form.Control type="numeric" placeholder="256" value={this.state.keyLength} onChange={(e) => this.setState({ keyLength: Number(e.target.value) })} />
+                                            </Form.Group>
+                                            <Button onClick={this.generateRSA}>Generate RSA Key</Button>
+                                        </>
+                                    }
+                                    {this.state.action === 'ECDSA-Gen' &&
+                                        <>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Curve</Form.Label>
+                                                <Form.Select value={this.state.curve} onChange={(e) => this.setState({ curve: e.target.value })}>
+                                                    <option value="P-256">P-256</option>
+                                                    <option value="P-384">P-384</option>
+                                                    <option value="P-521">P-521</option>
+                                                </Form.Select>
+                                            </Form.Group>
+                                            <Button onClick={this.generateRSA}>Generate ECDSA Key</Button>
+                                        </>
+                                    }
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col lg={5}>
+                            <Row style={{ height: '50%' }}>
+                                <Form.Group >
+                                    <Form.Label>Input</Form.Label>
+                                    <Form.Control as="textarea" rows={12} placeholder="Input"
+                                        value={this.state.input} onChange={e => this.setState({ input: e.target.value })} />
+                                </Form.Group>
+                            </Row>
+                            <Row style={{ height: '50%' }}>
+                                <Form.Group >
+                                    <Form.Label>Output</Form.Label>
+                                    <Form.Control as="textarea" rows={12} placeholder="Output" value={this.state.generatedKey} />
+                                </Form.Group>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Modal show={this.state.loading}>
+                        <Modal.Header closeButton>
+                            <Modal.Title></Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body style={{ textAlign: 'center' }}>
+                            <Spinner animation="border" role="status" >
+                                <span className="visually-hidden">Loading...</span>
+                            </Spinner>
+                        </Modal.Body>
+                        <Modal.Footer></Modal.Footer>
+                    </Modal>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
