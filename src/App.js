54940f45b67484c975289188cb69978fc044aa2f
Francesco <goras.francesco@gmail.com>
2022-07-29 00:12:05 +1200

    Basic menu setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="App" lg={6}>
+            <Container fluid>
+                <Row>
+                    <Col lg={3} style={{ padding: 0 }}>
+                        <Nav defaultActiveKey="AES" className="flex-column" style={{ backgroundColor: 'white', minHeight: '100vh' }} variant="pills">
+                            <h3 className="title">Crypto Tools</h3>
+                            <Nav.Item>Keys</Nav.Item>
+                            <Nav.Link eventKey="AES">AES</Nav.Link>
+                            <Nav.Link eventKey="RSA">RSA</Nav.Link>
+                            <Nav.Link eventKey="ECDSA">ECDSA</Nav.Link>
+                            <Nav.Item>Hashing</Nav.Item>
+                            <Nav.Link eventKey="SHA-1">SHA-1</Nav.Link>
+                            <Nav.Link eventKey="SHA-256">SHA-256</Nav.Link>
+                            <Nav.Link eventKey="SHA-384">SHA-384</Nav.Link>
+                            <Nav.Link eventKey="SHA-512">SHA-512</Nav.Link>
+                            <Nav.Item>Encoding</Nav.Item>
+                            <Nav.Link eventKey="Base64">Base64</Nav.Link>
+                            <Nav.Link eventKey="Hex">Hex</Nav.Link>
+                            <Nav.Link eventKey="Binary">Binary</Nav.Link>
+                        </Nav>
+                    </Col>
+                    <Col lg={9}>
+                        <h4> Generate Key </h4>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Keylength</Form.Label>
+                            <Form.Control type="numeric" placeholder="256" />
+                        </Form.Group>
+                        <Button>Generate</Button>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
