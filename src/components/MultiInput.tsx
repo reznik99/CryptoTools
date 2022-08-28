18d8b4cd53bba2b17c8aa4c866fea140e463b7c3
Francesco <goras.francesco@gmail.com>
2022-08-28 23:47:51 +1200

    MultiInput mobile friendly fixes

--- src/components/MultiInput.tsx
+++ src/components/MultiInput.tsx
@@ -22,31 +22,27 @@ function MultiInput(props: IProps) {
     return (
         <>
             { props.Rows.map( (row, idx) => (
-                <Row key={idx}>
-                    <Col>
-                        <Form.Group className="mb-1 d-flex">
-                            <Col lg md={6} sm={12} className="mx-1">
-                                <Form.Control type="text" placeholder="Extension Value" value={row.value} onChange={(e) => props.onChange(idx, {type: row.type, value: e.target.value})}/>
-                            </Col>
-                            <Col md sm={12} className="mx-1">
-                                <Form.Select placeholder="Extension Type" value={row.type} onChange={(e) => props.onChange(idx, {type: e.target.value, value: row.value})}>
-                                    <option value="DNSName">DNS Name</option>
-                                    <option value="IPAddress">IP Address</option>
-                                    <option value="EmailAddress">Email Address</option>
-                                    <option value="DirectoryName">Directory Name</option>
-                                    <option value="OtherName">Other Name</option>
-                                </Form.Select>
-                            </Col>
+                <Row key={idx} className="mb-2">
+                    <Col lg md={6} sm={12} className="my-1 pe-0">
+                        <Form.Control type="text" placeholder="Extension Value" value={row.value} onChange={(e) => props.onChange(idx, {type: row.type, value: e.target.value})}/>
+                    </Col>
+                    <Col lg md={6} sm={12} className="my-1 pe-0">
+                        <Form.Select placeholder="Extension Type" value={row.type} onChange={(e) => props.onChange(idx, {type: e.target.value, value: row.value})}>
+                            <option value="DNSName">DNS Name</option>
+                            <option value="IPAddress">IP Address</option>
+                            <option value="EmailAddress">Email Address</option>
+                            <option value="DirectoryName">Directory Name</option>
+                            <option value="OtherName">Other Name</option>
+                        </Form.Select>
+                    </Col>
 
-                            <Col lg="auto" md={12} className="mx-1">
-                                <Button variant="danger" onClick={() => props.DeleteRow(idx)} disabled={props.Rows.length <= 1}>
-                                    <i className="bi bi-x"/>
-                                </Button>
-                                <Button variant="success" onClick={() => props.AddRow()}>
-                                    <i className="bi bi-plus"/>
-                                </Button>
-                            </Col>
-                        </Form.Group>
+                    <Col lg="auto" className="my-1 pe-0">
+                        <Button className="me-1" variant="danger" onClick={() => props.DeleteRow(idx)} disabled={props.Rows.length <= 1}>
+                            <i className="bi bi-x"/>
+                        </Button>
+                        <Button variant="success" onClick={() => props.AddRow()}>
+                            <i className="bi bi-plus"/>
+                        </Button>
                     </Col>
                 </Row>
             ))}
