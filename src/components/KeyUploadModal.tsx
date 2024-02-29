091550aba249cd4059cdf87cabcfc2a50a8c5f33
Francesco <goras.francesco@gmail.com>
2024-03-01 01:33:20 +1300

    Added key upload modal to allow pasting or selecting file for private and public key

---
+++ src/components/KeyUploadModal.tsx
@@ -0,0 +1,61 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Stack, TextField } from "@mui/material";
+import React, { useState } from "react";
+import FileUploadBtn from "./FileUploadBtn";
+import { CloudUpload } from "@mui/icons-material";
+import { textfieldMonoStyle } from "./Output";
+
+
+type Props = {
+    open: boolean;
+    onClose: () => void;
+    onSubmit: (priv: string, pub: string) => void;
+}
+
+
+export function KeyUploadModal(props: Props) {
+
+    const [privateKey, setPrivateKey] = useState('')
+    const [publicKey, setPublicKey] = useState('')
+
+    return (
+        <Dialog open={props.open} onClose={props.onClose} maxWidth='lg' fullWidth>
+            <DialogTitle>Supply Keypair</DialogTitle>
+            <DialogContent>
+
+                <DialogContentText>
+                    Supply a Private and Public key in <strong>PKCS8 & PKIX</strong> formats to be used in the generation of the CSR.
+                </DialogContentText>
+
+                <Stack direction='row' spacing={2} marginY={2} width='100%'>
+                    <Stack direction='column' width='100%' spacing={2}>
+                        <FileUploadBtn onRead={(priv) => setPrivateKey(String(priv))} startIcon={<CloudUpload />} >
+                            Select Private Key
+                        </FileUploadBtn>
+                        <TextField fullWidth multiline rows={5}
+                            margin="dense"
+                            label="Private Key"
+                            inputProps={textfieldMonoStyle}
+                            value={privateKey}
+                            onChange={(e) => setPrivateKey(e.target.value)} />
+                    </Stack>
+                    <Stack direction='column' width='100%' spacing={2}>
+                        <FileUploadBtn onRead={(pub) => setPublicKey(String(pub))} startIcon={<CloudUpload />} >
+                            Select Public Key
+                        </FileUploadBtn>
+                        <TextField fullWidth multiline rows={5}
+                            margin="dense"
+                            label="Public Key"
+                            inputProps={textfieldMonoStyle}
+                            value={publicKey}
+                            onChange={(e) => setPublicKey(e.target.value)} />
+                    </Stack>
+                </Stack>
+
+            </DialogContent>
+            <DialogActions sx={{ justifyContent: 'space-around' }}>
+                <Button variant='outlined' color='warning' onClick={props.onClose}>Cancel</Button>
+                <Button variant='contained' color='primary' onClick={() => props.onSubmit(privateKey, publicKey)}>Ok</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
